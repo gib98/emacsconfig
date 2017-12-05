97a754c0b2dbd676e5af5ce30f5accb03107cf5b
Gibran Essa <gibran.essa@gmail.com>
2017-12-04 23:30:40 -0500

    im not quite sure

---
+++ elpa/vue-mode-20171013.1728/vue-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "vue-mode" "20171013.1728" "Major mode for vue component based on mmm-mode" '((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1") (edit-indirect "0.1.4")) :commit "0a6b7c226c5c0a26f3402cfe40169cb00f5d792f" :keywords '("languages"))
--- init.el
+++ init.el
@@ -80,3 +80,29 @@
               "~/.emacs.d/lisp/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+
+(require 'mmm-auto)
+(setq mmm-global-mode 'maybe)
+(mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
+(mmm-add-mode-ext-class 'html-mode "\\.html\\'" 'html-js)
+(mmm-add-mode-ext-class 'html-mode "\\.html\\'" 'html-css)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
